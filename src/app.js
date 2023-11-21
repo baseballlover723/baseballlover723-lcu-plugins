7a3f37c3d835338cfbcdb0fa1833963a35936af6
Philip Ross <dev@baseballlover723.com>
2023-11-20 23:47:40 -0800

    added the start queue plugin

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import LcuPluginManager from 'lcu-plugin-manager';
 import TahmKenchLcuPlugin from 'lcu-tahm-kench-plugin';
+import StartQueueLcuPlugin from 'lcu-start-queue-plugin';
 
-const pluginManager = new LcuPluginManager(new TahmKenchLcuPlugin());
+const pluginManager = new LcuPluginManager(new TahmKenchLcuPlugin(), new StartQueueLcuPlugin());
 
 pluginManager.start();
