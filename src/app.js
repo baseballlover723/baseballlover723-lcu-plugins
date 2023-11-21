68bed1e03e3aff7710ee90acdc807131efaef8d9
Philip Ross <dev@baseballlover723.com>
2023-11-21 02:14:19 -0800

    added request-party-owner plugin

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 import LcuPluginManager from 'lcu-plugin-manager';
 import TahmKenchLcuPlugin from 'lcu-tahm-kench-plugin';
 import StartQueueLcuPlugin from 'lcu-start-queue-plugin';
+import RequestPartyOwnerLcuPlugin from "lcu-request-party-owner-plugin";
 
-const pluginManager = new LcuPluginManager(new TahmKenchLcuPlugin(), new StartQueueLcuPlugin());
+const pluginManager = new LcuPluginManager(new TahmKenchLcuPlugin(), new StartQueueLcuPlugin(), new RequestPartyOwnerLcuPlugin());
 
 pluginManager.start();
