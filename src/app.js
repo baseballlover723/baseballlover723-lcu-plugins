2229a9419c35f25668ab4467099d9ccf72c9ae9f
Philip Ross <dev@baseballlover723.com>
2023-11-25 03:54:49 -0800

    update lcu-plugin-manager

--- src/app.js
+++ src/app.js
@@ -1,9 +1,15 @@
 import LcuPluginManager from 'lcu-plugin-manager';
 import TahmKenchLcuPlugin from 'lcu-tahm-kench-plugin';
 import StartQueueLcuPlugin from 'lcu-start-queue-plugin';
-import RequestPartyOwnerLcuPlugin from "lcu-request-party-owner-plugin";
-import RejectPartyOwnerLcuPlugin from "lcu-reject-party-owner-plugin";
+import RequestPartyOwnerLcuPlugin from 'lcu-request-party-owner-plugin';
+import RejectPartyOwnerLcuPlugin from 'lcu-reject-party-owner-plugin';
 
-const pluginManager = new LcuPluginManager(new TahmKenchLcuPlugin(), new StartQueueLcuPlugin(), new RequestPartyOwnerLcuPlugin(), new RejectPartyOwnerLcuPlugin());
+const plugins = [
+  new TahmKenchLcuPlugin(),
+  new StartQueueLcuPlugin(),
+  new RequestPartyOwnerLcuPlugin(),
+  new RejectPartyOwnerLcuPlugin(),
+];
+const pluginManager = new LcuPluginManager(plugins);
 
 pluginManager.start();
