d92eb5a8683aeba3376849f24c55b6684ad6ba39
Philip Ross <dev@baseballlover723.com>
2024-05-03 22:07:47 -0700

    updated to my version of ready up plugin

--- src/app.js
+++ src/app.js
@@ -3,12 +3,14 @@ import TahmKenchLcuPlugin from 'lcu-tahm-kench-plugin';
 import StartQueueLcuPlugin from 'lcu-start-queue-plugin';
 import RequestPartyOwnerLcuPlugin from 'lcu-request-party-owner-plugin';
 import RejectPartyOwnerLcuPlugin from 'lcu-reject-party-owner-plugin';
+import ReadyUpPlugin from "lcu-ready-up-plugin";
 
 const plugins = [
   new TahmKenchLcuPlugin(),
   new StartQueueLcuPlugin(),
   new RequestPartyOwnerLcuPlugin(),
   new RejectPartyOwnerLcuPlugin(false),
+  new ReadyUpPlugin()
 ];
 const pluginManager = new LcuPluginManager(plugins);
 
