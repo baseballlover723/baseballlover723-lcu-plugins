b2ef5467dcbd276c65503333545a468c19f92cbd
Philip Ross <dev@baseballlover723.com>
2025-03-08 16:51:23 -0800

    added randomize arena

--- src/app.js
+++ src/app.js
@@ -4,13 +4,15 @@ import StartQueueLcuPlugin from 'lcu-start-queue-plugin';
 import RequestPartyOwnerLcuPlugin from 'lcu-request-party-owner-plugin';
 import RejectPartyOwnerLcuPlugin from 'lcu-reject-party-owner-plugin';
 import ReadyUpPlugin from "lcu-ready-up-plugin";
+import RandomizeArenaLcuPlugin from "lcu-arena-random-plugin/src/randomizeArenaLcuPlugin.js";
 
 const plugins = [
   new TahmKenchLcuPlugin(),
   new StartQueueLcuPlugin(),
   new RequestPartyOwnerLcuPlugin(),
   new RejectPartyOwnerLcuPlugin(false),
-  new ReadyUpPlugin()
+  new ReadyUpPlugin(),
+  new RandomizeArenaLcuPlugin(false)
 ];
 const pluginManager = new LcuPluginManager(plugins);
 
